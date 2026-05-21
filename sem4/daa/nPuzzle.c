1e7e4a732b058e2d31a1a06e0d1d5fa55244dca5
Induaditya3 <o0bvc52p@duck.com>
2026-05-22 05:18:44 +0530

    add partial code to nPuzzle

---
+++ sem4/daa/nPuzzle.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#define MAX_LENGTH 1000
+
+typedef struct{
+  char act;
+  int val;
+}act_val;
+
+char solution[MAX_LENGTH];
+int top = 0;
+
+int cmpActs(const void *a, const void *b){
+  int vala = ((act_val*)a)->val;
+  int valb = ((act_val*)b)->val;
+  return vala - valb;
+}
+
+char opposite(char act){
+  if (act =='u') return 'd';
+  if (act =='d') return 'u';
+  if (act == 'l') return 'r';
+  if (act =='r') return 'l';
+  return ' ';
+}
+
+int value(int n, int board[n][n]){
+  int sum = 0;
+  int incorrect = 0;
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+      sum++;
+      if (i+j == 2*n-2 && board[i][j] == 0) break;
+      if (sum != board[i][j])
+        incorrect++;
+    }
+  }
+  return incorrect;
+}
+
+// moves the blank cell and modifies i, j to reflect new position of blank cell
+// return 0 if move made, -1 if illegal move is made
+int move(int n, int board[n][n], char act, int* i, int* j){
+  int old_i = *i;
+  int old_j = *j;
+  int temp;
+  if (act =='u' && old_i -1 >= 0){
+    *i = old_i -1;
+    temp = board[old_i][old_j];
+    board[old_i][old_j] = board[*i][*j];
+    board[*i][*j] = temp;
+  }
+  else if (act == 'd' && old_i +1 < n){
+    *i = old_i +1;
+    temp = board[old_i][old_j];
+    board[old_i][old_j] = board[*i][*j];
+    board[*i][*j] = temp;
+  }
+  else if (act == 'l' && old_j-1 >= 0){
+    *j = old_j -1;
+    temp = board[old_i][old_j];
+    board[old_i][old_j] = board[*i][*j];
+    board[*i][*j] = temp;
+  }
+  else if (act == 'r' && old_j +1 < n){
+    *j = old_j +1;
+    temp = board[old_i][old_j];
+    board[old_i][old_j] = board[*i][*j];
+    board[*i][*j] = temp;
+  }
+  else {
+    printf("Not valid action. So no action taken!\n");
+    return -1;
+  }
+  return 0;
+}
+
+void print_solution(){
+  for (int i = 0; i < top; i++){
+    printf("%c\n", solution[i]);
+  }
+}
+void solve(int n, int board[n][n], char prev_act, int i, int j){
+  if (value(n, board) == 0){
+    print_solution();
+    return;
+  }
+  act_val actions[4];
+  int no_actions = 0;
+  
+  // find merits of all actions
+  if ('u' != prev_act && move(n, board, 'u', &i, &j) == 0){
+    actions[no_actions++] = (act_val){.act = 'u', .val = value(n, board)};
+    move(n, board, 'd', &i, &j);
+  }
+  
+  if ('d' != prev_act && move(n, board, 'd', &i, &j) == 0){
+    actions[no_actions++] = (act_val){.act = 'd', .val = value(n, board)};
+    move(n, board, 'u', &i, &j);
+  }
+
+  if ('l' != prev_act && move(n, board, 'l', &i, &j) == 0){
+    actions[no_actions++] = (act_val) {.act = 'l', .val = value(n, board)};
+    move(n, board, 'r', &i, &j);
+  }
+  
+  if ('r' != prev_act && move(n, board, 'r', &i, &j) == 0){
+    actions[no_actions++] = (act_val) {.act = 'r', .val = value(n, board)};
+    move(n, board, 'l', &i, &j);
+  }
+
+  // sort the actions based on value in ascending
+  qsort(actions, no_actions, sizeof(act_val), cmpActs);
+
+  // make most promising moves first
+  for (int k = 0; k < no_actions; k++){
+    move(n, board, actions[k].act, &i, &j);
+    solution[top++] = actions[k].act;
+    solve(n, board, actions[k].act, i, j);
+    top--;
+    move(n, board, opposite(actions[k].act), &i, &j);
+  }
+}
+
+int main(){
+  const int n = 2;
+  
+  int board[n][n]; 
+  
+  board[0][0] = 2; board[0][1] = 0;
+  board[1][0] = 1; board[1][1] = 3;
+  solve(n, board, ' ', 0, 1);
+  return 0;
+}
